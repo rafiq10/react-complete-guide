000fc0f2c3233c7dc6eef40e3cbbb97d9a3ee78d
rafiq10 <rafiq10@gmail.com>
2018-03-28 10:54:11 +0200

    before deleting radium features

--- src/App.js
+++ src/App.js
@@ -1,67 +1,104 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
+
 import Person from './Person/Person';
 
 class App extends Component {
   state = {
     persons: [
-      {name: "Rafa", age: 34},
-      {name: "Vero", age: 32},
-      {name: "Bartek", age: 33}
+      {id: "1", name: "Rafa", age: 34},
+      {id: "2",name: "Vero", age: 32},
+      {id: "3",name: "Bartek", age: 33}
     ],
+    showPersons: false,
   }
 
-  switcNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        {name: newName, age: 34},
-        {name: "Vero", age: 32},
-        {name: "Bartek", age: 25}
-      ],
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]}
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
+  }
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: "Grzesio", age: 34},
-        {name: "Vero", age: 32},
-        {name: event.target.value, age: 25}
-      ],
-    })
-  }
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  } 
+
    render() {
      const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
+                      backgroundColor: 'green',
+                      color: 'white',
+                      font: 'inherit',
+                      border: '1px solid blue',
+                      padding: '8px',
+                      cursor: 'pointer',
+                      ':hover':{
+                        backgroundColor: 'lightgreen',
+                        color: 'black',
+                      }
+                    }
 
+     let persons = null;
+
+     if(this.state.showPersons){
+       persons = (
+          <div>
+            {this.state.persons.map((person,index) => {
+              return <Person 
+                        click={() => this.deletePersonHandler(index)}
+                        name={person.name} 
+                        age={person.age} 
+                        key={person.id}
+                        changed={(event) => this.nameChangedHandler(event, person.id)} />
+            })}
+          </div> 
+       );
+       style.backgroundColor = 'red';
+       style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black',
+      }
      }
 
+     let classes = [];
+     if (this.state.persons.length<=2){
+       classes.push('red');
+     }
+     if (this.state.persons.length<=1){
+       classes.push('bold');
+     }
+     
+
+
     return (
+      <Radium.StyleRoot>
       <div className="App">
         <h1>Hi, I am a react App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style} 
-          onClick={this.switcNameHandler.bind(this, 'Grzegorz')}>Switch Name</button>
+          onClick={this.togglePersonsHandler}>
+          Toggle Persons</button>
           <br /><br />
-
-        <button
-          style={style} 
-          onClick={() => {this.switcNameHandler("Gregorios")} }>Switch Name2</button>
-        <Person 
-          name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person click={this.switcNameHandler.bind(this, 'Bilu')}
-          name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person 
-          changed={this.nameChangeHandler}
-          name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          {persons}
       </div>
+      </Radium.StyleRoot>
+
     );
-  // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'I am a react App'));
   }
 }
 
-export default App;
+export default Radium(App);
